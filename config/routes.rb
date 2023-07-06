41fa11629a4797f43b3ae51e357bb4795af0532e
Ankit Singh <1999ankits@gmail.com>
2023-07-06 23:59:26 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 require 'sidekiq/web'
 Sidekiq::Web.use ActionDispatch::Cookies
-Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_index_session"
+Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_index_key_session"
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root "welcome#show"
   
   mount Sidekiq::Web => "/sidekiq"
